03687690e12086f0fffce47d397905509b2af3b2
Fattah Muhyiddeen <fattahmuhyiddeen@gmail.com>
2023-04-26 07:34:38 +0800

    create button to clear filter

--- App.tsx
+++ App.tsx
@@ -1,6 +1,4 @@
-// Assumptions: no city has 2 parent districts and no district has 2 parent states
-
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -11,32 +9,44 @@ import Select from './components/Select';
 import Button from './components/Button';
 import data from './data';
 
+const mapData = (key: 'state' | 'city' | 'district') => data.reduce((a: string[], { [key]: d }) => a.includes(d) ? a : [...a, d], []).map(d => ({ label: d, value: d }))
+
 function App(): JSX.Element {
   const [state, setState] = useState('');
   const [district, setDistrict] = useState('');
   const [city, setCity] = useState('');
   const [searchResult, setSearchResult] = useState('');
 
-  useEffect(() => {
-    setDistrict('');
-    setCity('')
-  }, [state]);
-
-  useEffect(() => {
-    setCity('')
-  }, [district]);
-
   const search = () => {
     setSearchResult(`Search results for ${city || district || state || 'all'}`);
   }
 
+  const changeState = (s:string) =>{
+    setState(s);
+    setDistrict('');
+    setCity('');
+  }
+
+  const changeDistrict = (d:string)=>{
+    setDistrict(d);
+    setCity('');
+  }
+
+  const clearFilter = () => {
+    setState('');
+    setDistrict('');
+    setCity('');
+  }
+
   return (
-    <SafeAreaView style={{ margin: 10, marginTop: 50 }}>
-      <Text>Filter</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={{ flexDirection: 'row' }}>
+        {!!state ? <Button onPress={clearFilter}><Text>Clear filter</Text></Button> : <Text>Filter</Text>}
+      </View>
       <View style={{ flexDirection: 'row', gap: 5, marginBottom: 30 }}>
-        <Select data={data.reduce((a: string[], { state: d }) => a.includes(d) ? a : [...a, d], []).map(d => ({ label: d, value: d }))} onChange={setState} selected={state} />
-        {!!state && <Select data={data.filter(d => d.state === state).reduce((a: string[], { district: d }) => a.includes(d) ? a : [...a, d], []).map(d => ({ label: d, value: d }))} onChange={setDistrict} selected={district} />}
-        {!!district && <Select data={data.filter(d => d.district === district).reduce((a: string[], { city: d }) => a.includes(d) ? a : [...a, d], []).map(d => ({ label: d, value: d }))} onChange={setCity} selected={city} />}
+        <Select data={mapData('state')} onChange={changeState} selected={state} />
+        {!!state && <Select data={mapData('district')} onChange={changeDistrict} selected={district} />}
+        {!!district && <Select data={mapData('city')} onChange={setCity} selected={city} />}
         <Button style={styles.searchButton} onPress={search}><Text>Search</Text></Button>
       </View>
       <Text>{searchResult}</Text>
@@ -45,12 +55,13 @@ function App(): JSX.Element {
 }
 
 const styles = StyleSheet.create({
-searchButton:{
-  backgroundColor: 'lightblue',
-  paddingHorizontal: 20,
-  borderRadius:10,
-  marginLeft: 20
-}
+  container: { margin: 10, marginTop: 50 },
+  searchButton: {
+    backgroundColor: 'lightblue',
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    marginLeft: 20
+  }
 });
 
 export default App;
